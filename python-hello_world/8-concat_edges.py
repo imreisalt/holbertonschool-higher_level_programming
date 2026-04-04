fdfadcc85a2bcc3af219b98cfe568086f8467b37
Imran <imranqedimov@gmail.com>
2026-04-04 20:29:13 +0000

    Fix python-hello_world task 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-word = "Python is an interpreted, interactive, object-oriented programming language with a very simple syntax"
-print(word[39:] + word[34:38] + word[:6] + word[7:8] + word[28:34] + word[38:39] + word[:6])
+word = "Holberton School"
+number = 98
+language = "Python"
+print(word[8:] + "-oriented programming with " + language)
