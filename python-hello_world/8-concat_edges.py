af02993633d6d35b58d85a4ee4f2c9a6ee37a030
Imran <imranqedimov@gmail.com>
2026-04-04 20:25:37 +0000

    Add python-hello_world task 6

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+word = "Python is an interpreted, interactive, object-oriented programming language with a very simple syntax"
+print(word[39:] + word[34:38] + word[:6] + word[7:8] + word[28:34] + word[38:39] + word[:6])
