a1ac2057bb78cbbcb27490676ff9ed9066a4e85f
Imran <imranqedimov@gmail.com>
2026-04-18 11:25:43 +0000

    Fix python-network_1 task 5 pycodestyle

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,9 +1,1 @@
-#!/usr/bin/python3
-"""Script that fetches a URL and displays the X-Request-Id header value"""
-import requests
-import sys
-
-if __name__ == "__main__":
-    response = requests.get(sys.argv[1])
-    print(response.headers.get('X-Request-Id'))
-
+cat -A 5-hbtn_header.py
